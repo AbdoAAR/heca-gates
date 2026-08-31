8cb8d777d9453dfa0b5829070a10549fd0efaeb8
AbdoAAR <abdoqha@gmail.com>
2026-08-31 20:07:07 +0300

    Update dllmain.cpp

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -3,16 +3,19 @@
 
 static GateOccupancyPlugin* g_plugin = nullptr;
 
-BOOL APIENTRY DllMain(HMODULE, DWORD, LPVOID) { return TRUE; }
+BOOL APIENTRY DllMain(HMODULE, DWORD, LPVOID)
+{
+    return TRUE;
+}
 
-extern "C" __declspec(dllexport)
-void EuroScopePlugInInit(EuroScopePlugIn::CPlugIn** ppPlugInInstance) {
+void EuroScopePlugInInit(EuroScopePlugIn::CPlugIn** ppPlugInInstance)
+{
     g_plugin = new GateOccupancyPlugin();
     *ppPlugInInstance = g_plugin;
 }
 
-extern "C" __declspec(dllexport)
-void EuroScopePlugInExit(void) {
+void EuroScopePlugInExit(void)
+{
     delete g_plugin;
     g_plugin = nullptr;
 }
